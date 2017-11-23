aedd2ab47de41673c5c62ea0dfeffa2d6d6f8453
Rick Hornsby <richard.hornsby@espus.com>
2017-11-23 12:57:11 -0600

    Initial add of setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+install_requires = ['pygame', 'mutagen']
+
+setup(
+    name='mirror_engine',
+    version='1.0.0',
+    packages=[],
+    install_requires=install_requires,
+    url='https://github.com/rjhornsby/mirror_engine',
+    license='',
+    author='rhornsby',
+    author_email='',
+    description=''
+)
